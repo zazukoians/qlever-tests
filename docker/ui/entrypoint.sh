c4aab7fe8b5281f918ebe07c176409babcb13fd4
Ludovic Muller <ludovicmuller1@gmail.com>
2024-07-16 17:57:57 +0200

    ui: update db before starting the UI

--- docker/ui/entrypoint.sh
+++ docker/ui/entrypoint.sh
@@ -1,22 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 # Some configuration
 QLEVER_UI_PORT="${QLEVER_UI_PORT:-7002}"
 QLEVER_UI_WORKERS="${QLEVER_UI_WORKERS:-3}"
 QLEVER_UI_LIMIT_REQUEST_LINE="${QLEVER_UI_LIMIT_REQUEST_LINE:-10000}"
 
-# Allow the script to use jobs (`m` flag)
-set -euxm
-
-# Start the UI in the background
-gunicorn \
-  --bind ":${QLEVER_UI_PORT}" \
-  --workers "${QLEVER_UI_WORKERS}" \
-  --limit-request-line "${QLEVER_UI_LIMIT_REQUEST_LINE}" \
-  qlever.wsgi:application &
+# Make the script fail on errors
+set -eux
 
 # Configure the dataset
 python manage.py configure default "${QLEVER_SERVER_ENDPOINT}"
 
-# Put the UI process in the foreground to keep the container running
-fg %1
+# Start the UI
+gunicorn \
+  --bind ":${QLEVER_UI_PORT}" \
+  --workers "${QLEVER_UI_WORKERS}" \
+  --limit-request-line "${QLEVER_UI_LIMIT_REQUEST_LINE}" \
+  qlever.wsgi:application
